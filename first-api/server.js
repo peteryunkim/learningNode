a0c6720b190b6d4b242a9eaffc1b0f1cb21e67a8
peteryunkim <peteryunkim@gmail.com>
2017-08-22 12:12:05 -0400

    first server running

--- first-api/server.js
+++ first-api/server.js
@@ -1,1 +1,14 @@
-import express from 'express'
+const express = require('express')
+let app = express()
+
+app.get('/', (request, response) => {
+	response.send('Getting first API')
+})
+
+app.get('/jp', (req, res)=>{
+	res.send({name: "WOFF", system: 'Ps4'})
+})
+
+app.listen(3000, () => {
+	console.log('first api running on port 3000')
+})
