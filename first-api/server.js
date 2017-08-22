d7813a4ebc2d825362c71d467d5041eb11840318
peteryunkim <peteryunkim@gmail.com>
2017-08-22 15:42:01 -0400

    CRU done

--- first-api/server.js
+++ first-api/server.js
@@ -1,8 +1,58 @@
 const express = require('express')
 let app = express()
 
-app.get('/', (request, response) => {
-	response.send('Getting first API')
+let toDo = [
+	{
+		id: 1,
+		"text": 'cut watermelons'
+	},
+	{
+		id: 2,
+		text: 'do dishes'
+	},
+	{
+		id: 3,
+		text: 'call Vin'
+	}
+]
+
+app.get('/tasks', (request, response) => {
+	response.send(toDo)
+})
+
+app.post('/tasks', (request, response) => {
+	let task = request.body
+	if (!task){
+		response.status(500).send({error:'task does not have text'})
+	} else {
+		toDo.push(task)
+		response.status(200).send(toDo)
+	}
+})
+
+//Update
+app.put('/tasks/:id', (request, response) => {
+	let newText = request
+	console.log(request)
+
+	if (!newText || newText === ""){
+		response.status(500).send({error: 'You must have a text for task'})
+	} else {
+		let found = false
+		for (i=0; i < toDo.length; i++){
+			let t = toDo[i]
+			if (t.id === request.params.id){
+				t.text = newText
+				found = true
+				break
+			}
+		}
+		if (!found){
+			response.status(500).send({error:'Not found'})
+		} else {
+			response.send(toDo)
+		}
+	}
 })
 
 app.get('/jp', (req, res)=>{
